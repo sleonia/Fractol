f07ddcfb5a380da9427c77c1238801b8fab5334f
Sherise Leonia <sleonia@at-a2.21-school.ru>
2019-08-16 21:04:24 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+![Example](https://github.com/EmbodimentEvil/fractol/raw/master/image/julia1.png)
+---
+# Project for school_21.
+## FRACTOL.
+---
+##### In this project I make a program which that displays 2D mathematic fractol.
+---
+## Compiling
+Use `make` and run `./fractol [0-6]`.
+
+!![Example](https://github.com/EmbodimentEvil/fractol/raw/master/image/julia2.png)
+![Example](https://github.com/EmbodimentEvil/fractol/raw/master/image/spider.png)
--- source/list.c
+++ source/list.c
@@ -6,7 +6,7 @@
 /*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 11:55:26 by sleonia           #+#    #+#             */
-/*   Updated: 2019/08/16 17:34:40 by sleonia          ###   ########.fr       */
+/*   Updated: 2019/08/16 21:00:29 by sleonia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,6 @@ void				default_value(t_fractol *f)
 	f->crdn->move_x = 1;
 	f->crdn->move_y = 1;
 	f->repeats = 200;
-	f->move_flag = 0;
 	f->crdn->rotation_x = -0.5;
 	f->crdn->rotation_y = 0.5;
 }
